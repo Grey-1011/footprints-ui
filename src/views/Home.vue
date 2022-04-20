a0172a7d835014be2bc7c0829b62f57a5ab22172
grey <z1251543107@gmail.com>
2022-04-20 17:10:49 +0800

    Home 页面的 css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <Topnav />
     <div class="banner">
-      <h1>轱辘UI</h1>
+      <h1>No NameUI</h1>
       <h2>一个厉害的 UI框架</h2>
       <p class="actions">
         <a href="https://github.com">GitHub</a>
@@ -20,4 +20,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+
 </style>
