42e27311ad1b7baad92f50d712bd33be4cdeda5c
grey <z1251543107@gmail.com>
2022-04-22 23:16:32 +0800

    Button 基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,24 +1,55 @@
 <template>
-  <div class="all-button" :class="`theme-${theme}`">
-    <button>
-      <slot />
-    </button>
-  </div>
+  <button class="all-button" :class="`theme-${theme}`">
+    <slot />
+  </button>
 </template>
 
 <script lang="ts">
 export default {
- props:{
-     theme: {
-         type: String,
-         default: 'button'
-     }
- }
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-div {
-  border: 1px solid red;
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.all-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out($color: #000000, $amount: 0.95);
+
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
 }
 </style>
