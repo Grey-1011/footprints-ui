9d8733c5b11d530127be10b7930f6929d2819313
grey <z1251543107@gmail.com>
2022-04-25 00:48:54 +0800

    openDialog 一句代码打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
-    DialogDemo page
-    <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="visible" :closeOnClickOverlay="false" :ok="fn1" :cancel="fn2">
-        <template v-slot:content>
-            <div>some contents...</div>
-            <div>some contents...</div>
-        </template>
-        <template v-slot:title>
-            <strong>title</strong>
-        </template>        
+    <h2>Dialog 对话框</h2>
+    <h3>基本</h3>
+    <Button @click="toggle">Open Dialog</Button>
+    <Dialog
+      v-model:visible="visible"
+      :closeOnClickOverlay="false"
+      :ok="fn1"
+      :cancel="fn2"
+    >
+      <template v-slot:content>
+        <div>some contents...</div>
+        <div>some contents...</div>
+      </template>
+      <template v-slot:title>
+        <strong>title</strong>
+      </template>
     </Dialog>
+    <h3>一键打开</h3>
+    <Button @click="showDialog">Open Dialog</Button>
   </div>
 </template>
 
@@ -18,6 +26,8 @@
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
 import { ref } from "vue";
+
+import { openDialog } from "../lib/openDialog";
 
 export default {
   components: { Dialog, Button },
@@ -30,7 +40,19 @@ export default {
       return false;
     };
     const fn2 = () => {};
-    return { visible, toggle, fn1, fn2 };
+    const showDialog = () => {
+        openDialog({
+            title:"Hello Dialog",
+            content:"一键打开Dialog",
+            ok(){
+                console.log("ok")
+            },
+            cancel(){
+                console.log("cancel");
+            }
+        })
+    };
+    return { visible, toggle, fn1, fn2, showDialog };
   },
 };
 </script>
