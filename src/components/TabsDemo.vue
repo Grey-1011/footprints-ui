ef334aa656b662fc8166408cb72df1eec88b3de5
grey <z1251543107@gmail.com>
2022-04-25 22:00:11 +0800

    实现tab 和 内容的同步切换

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Tabs 标签页</h3>
-    <Tabs>
+    <Tabs v-model:selected="x">
       <Tab title="tab1">content 1</Tab>
       <Tab title="tab2">content 2</Tab>
     </Tabs>
@@ -11,7 +11,12 @@
 <script lang="ts">
 import Tabs from "../lib/Tabs.vue";
 import Tab from "../lib/Tab.vue";
+import { ref } from "vue";
 export default {
   components: { Tabs, Tab },
+  setup() {
+    const x = ref("tab1");
+    return { x };
+  },
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="no-tabs">
+    <div class="no-tabs-nav">
+      <div class="no-tabs-nav-item" :class="{selected:t === selected}" v-for="(t, index) in titles" :key="index" @click="select(t)">
+        {{ t }}
+      </div>
+    </div>
+    <div class="no-tabs-content">
+      <component
+        class="no-tabs-content-item"
+        :is="current" :key="selected" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import { computed } from 'vue';
 import Tab from "./Tab.vue";
 export default {
+    props:{
+        selected:{
+            type:String,
+        }
+    },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -15,10 +29,51 @@ export default {
         throw new Error("Tabs 子标签必须是 Tab");
       }
     });
-    const titles = defaults.map((tag)=>{
-        return tag.props.title
-    })
-    return { defaults,titles };
+
+    const current = computed(()=>{
+        return defaults.filter((tag)=>{
+            return tag.props.title === props.selected
+        })[0]
+    }) 
+    
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+
+    const select = (title:String)=>{
+        context.emit("update:selected",title)
+    }
+    return { defaults, titles,current,select };
   },
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.no-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
