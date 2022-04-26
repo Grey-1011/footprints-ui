ce1599f60cfd51101ab7a80130811db231cb84b9
grey <z1251543107@gmail.com>
2022-04-26 17:54:32 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -8,11 +8,7 @@
         :key="index"
         @click="select(t)"
         :ref="
-          (el) => {
-            if (el) navItems[index] = el;
-          }
-        "
-      >
+          (el) => {if (t===selected) selectedItem = el;}">
         {{ t }}
       </div>
       <div class="no-tabs-nav-indicator" ref="indicator"></div>
@@ -33,23 +29,18 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]); // 数组的默认值 []
+    const selectedItem = ref<HTMLDivElement>(null);
+    // const navItems = ref<HTMLDivElement[]>([]); // 数组的默认值 []
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
 
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      // 过滤出 被选中的 div
-      console.log(result);
-      const { width } = result.getBoundingClientRect();
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       // 获取 container 的left
       const { left: left1 } = container.value.getBoundingClientRect();
       // 获取当前 div 的 left
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
@@ -80,7 +71,7 @@ export default {
       titles,
       current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
     };
