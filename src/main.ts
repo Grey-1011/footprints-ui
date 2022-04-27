072e050a1f4f70cb2b7873e9b9f1b39807834344
grey <z1251543107@gmail.com>
2022-04-27 23:29:48 +0800

    填充文档内容， 引入github 样式

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './index.scss'
 import { createApp, h } from 'vue'
 import App from './App.vue'
 import { router } from './router' // 把router 封装成单独的 ts
-
+import 'github-markdown-css'
 
 const app = createApp(App)
 app.use(router)
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,3 +1,19 @@
 <template>
-    <div>Install</div>
+    <article class="markdown-body"> 
+        <h1>安装</h1> 
+        <p>打开终端运行下列命令：</p > 
+        <pre><code>npm install footprints-ui</code></pre> 
+        <p>或</p > 
+        <pre><code>yarn add footprints-ui</code></pre> 
+  
+        <p> 
+            下一节：<a href="#/doc/Get-started">开始使用</ a> 
+        </p > 
+  </article> 
 </template>
+
+<style lang="scss">
+.markdown-body{
+    padding-left: 8px;
+}
+</style>
