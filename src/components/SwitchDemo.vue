c30ee73217c4b7bf4f3e067b36b9fecd0c41fdf4
grey <z1251543107@gmail.com>
2022-04-28 20:07:05 +0800

    删掉无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -2,40 +2,18 @@
   <div>
     <h1>Switch</h1>
     <Demo :component="Switch1Demo"/>
-    <hr>
-    <div class="demo">
-      <h2>常规用法</h2>
-      <div class="demo-component">
-        <component :is="Switch1Demo"/>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')" />
-      </div>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Demo from './Demo.vue'
-import { ref } from "vue";
-import Switch from "../lib/Switch.vue";
-import Button from "../lib/Button.vue";
-import Switch1Demo from "./Switch1.demo.vue";
-// console.log(Switch1Demo.__sourceCode);
-import 'prismjs';
-import 'prismjs/themes/prism.css'
-const Prism = (window as any).Prism
-console.log(Prism);
 
+import Switch1Demo from "./Switch1.demo.vue";
 
 export default {
-  components: { Button, Demo },
+  components: { Demo },
   setup() {
-    const bool = ref(false);
-    return { bool, Switch1Demo, Prism };
+    return { Switch1Demo };
   },
 };
 </script>
